44d87be9ca29bd99b8d8548dc67424a511b018d7
Agohige-Tech <60114461+Agohige-Tech@users.noreply.github.com>
2022-10-08 23:12:37 -0400

    feat(comments): Implemented comment form section

--- pages/unit/[slug].tsx
+++ pages/unit/[slug].tsx
@@ -3,22 +3,113 @@ import React from "react";
 import Header from "../../components/Header";
 import { sanityClient, urlFor } from "../../sanity";
 import { Unit } from "../../typings";
+import { useForm, SubmitHandler } from "react-hook-form";
 
 interface Props {
   unit: Unit;
 }
 
-const textColor = {
-  "United States Army": "text-green-600",
-  "United States Marine Corps": "text-red-500",
-  "United States Navy": "text-blue-500",
-  "United States Air Force": "text-blue-400",
-  "United States Coast Guard": "text-orange-500",
-  "United States Space Force": "text-blue-500",
-  "United States National Guard": "text-black",
+interface FormInput {
+  _id: string;
+  name: string;
+  email: string;
+  comment?: string;
+}
+
+const styles = {
+  textColor: {
+    "United States Army": "text-green-600",
+    "United States Marine Corps": "text-red-500",
+    "United States Navy": "text-blue-500",
+    "United States Air Force": "text-blue-400",
+    "United States Coast Guard": "text-orange-500",
+    "United States Space Force": "text-blue-500",
+    "United States National Guard": "text-black",
+  },
+  lineStyle: {
+    "United States Army": "max-w-lg mx-auto border my-5 border-green-600",
+    "United States Marine Corps": "max-w-lg mx-auto border my-5 border-red-500",
+    "United States Navy": "max-w-lg mx-auto border my-5 border-blue-500",
+    "United States Air Force": "max-w-lg mx-auto border my-5 border-blue-400",
+    "United States Coast Guard":
+      "max-w-lg mx-auto border my-5 border-orange-500",
+    "United States Space Force": "max-w-lg mx-auto border my-5 border-blue-500",
+    "United States National Guard": "max-w-lg mx-auto border my-5 border-black",
+  },
+  form: {
+    label: "block mb-5",
+    span: "text-gray-700",
+    inputs: {
+      "United States Army":
+        "ring-green-600 shadow border rounded py-2 px-3 form-input mt-1 block w-full outline-none focus:ring-1",
+      "United States Marine Corps":
+        "ring-red-500 shadow border rounded py-2 px-3 form-input mt-1 block w-full outline-none focus:ring-1",
+      "United States Navy":
+        "ring-blue-500 shadow border rounded py-2 px-3 form-input mt-1 block w-full outline-none focus:ring-1",
+      "United States Air Force":
+        "ring-blue-400 shadow border rounded py-2 px-3 form-input mt-1 block w-full outline-none focus:ring-1",
+      "United States Coast Guard":
+        "ring-orange-500 shadow border rounded py-2 px-3 form-input mt-1 block w-full outline-none focus:ring-1",
+      "United States Space Force":
+        "ring-blue-500 shadow border rounded py-2 px-3 form-input mt-1 block w-full outline-none focus:ring-1",
+      "United States National Guard":
+        "ring-black shadow border rounded py-2 px-3 form-input mt-1 block w-full outline-none focus:ring-1",
+    },
+    textarea: {
+      "United States Army":
+        "ring-green-600 shadow border rounded py-2 px-3 form-textarea mt-1 block w-full outline-none focus:ring-1",
+      "United States Marine Corps":
+        "ring-red-500 shadow border rounded py-2 px-3 form-textarea mt-1 block w-full outline-none focus:ring-1",
+      "United States Navy":
+        "ring-blue-500 shadow border rounded py-2 px-3 form-textarea mt-1 block w-full outline-none focus:ring-1",
+      "United States Air Force":
+        "ring-blue-400 shadow border rounded py-2 px-3 form-textarea mt-1 block w-full outline-none focus:ring-1",
+      "United States Coast Guard":
+        "ring-orange-500 shadow border rounded py-2 px-3 form-textarea mt-1 block w-full outline-none focus:ring-1",
+      "United States Space Force":
+        "ring-blue-500 shadow border rounded py-2 px-3 form-textarea mt-1 block w-full outline-none focus:ring-1",
+      "United States National Guard":
+        "ring-black shadow border rounded py-2 px-3 form-textarea mt-1 block w-full outline-none focus:ring-1",
+    },
+    submit: {
+      "United States Army":
+        "shadow text-white bg-green-600 hover:bg-green-500 focus:shadow-outline focus:outline-none font-bold py-2 px-4 rounded cursor-pointer",
+      "United States Marine Corps":
+        "shadow text-white bg-red-500 hover:bg-red-400 focus:shadow-outline focus:outline-none font-bold py-2 px-4 rounded cursor-pointer",
+      "United States Navy":
+        "shadow text-white bg-blue-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none font-bold py-2 px-4 rounded cursor-pointer",
+      "United States Air Force":
+        "shadow text-white bg-blue-400 hover:bg-blue-300 focus:shadow-outline focus:outline-none font-bold py-2 px-4 rounded cursor-pointer",
+      "United States Coast Guard":
+        "shadow text-white bg-orange-500 hover:bg-orange-400 focus:shadow-outline focus:outline-none font-bold py-2 px-4 rounded cursor-pointer",
+      "United States Space Force":
+        "shadow text-white bg-blue-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none font-bold py-2 px-4 rounded cursor-pointer",
+      "United States National Guard":
+        "shadow text-white bg-black hover:bg-gray focus:shadow-outline focus:outline-none font-bold py-2 px-4 rounded cursor-pointer",
+    },
+  },
 };
 
 function Unit({ unit }: Props) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormInput>();
+
+  const onSubmit: SubmitHandler<FormInput> = async (data) => {
+    await fetch("/api/comment", {
+      method: "POST",
+      body: JSON.stringify(data),
+    })
+      .then(() => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <main>
       <Header />
@@ -38,7 +129,9 @@ function Unit({ unit }: Props) {
             <h1 className="text-3xl flex items-end">{unit.title}</h1>
           </div>
           <p className="font-extralight text-sm">
-            <span className={textColor[unit.branch.name as keyof object]}>
+            <span
+              className={styles.textColor[unit.branch.name as keyof object]}
+            >
               {unit.branch.name}
             </span>
           </p>
@@ -47,6 +140,61 @@ function Unit({ unit }: Props) {
           {unit.description}
         </h2>
       </article>
+      <hr className={styles.lineStyle[unit.branch.name as keyof object]} />
+
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col p-5 max-w-2xl mx-auto mb-10"
+      >
+        <h3 className="text-sm">
+          <span className={styles.textColor[unit.branch.name as keyof object]}>
+            Been stationed here?
+          </span>
+        </h3>
+        <h4 className="text-3xl font-bold">Leave a Rating below!</h4>
+        <hr className="py-3 mt-2" />
+
+        <input {...register("_id")} type="hidden" name="_id" value={unit._id} />
+        <label className={styles.form.label}>
+          <span className={styles.form.span}>Name</span>
+          <input
+            {...register("name", { required: true })}
+            className={styles.form.inputs[unit.branch.name as keyof object]}
+            placeholder="Your Name Here"
+            type="text"
+          />
+        </label>
+        <label className={styles.form.label}>
+          <span className={styles.form.span}>Email</span>
+          <input
+            {...register("email", { required: true })}
+            className={styles.form.inputs[unit.branch.name as keyof object]}
+            placeholder="Your Email Here"
+            type="email"
+          />
+        </label>
+        <label className={styles.form.label}>
+          <span className={styles.form.span}>Comment</span>
+          <textarea
+            {...register("comment")}
+            className={styles.form.textarea[unit.branch.name as keyof object]}
+            placeholder="Your Comment Here"
+            rows={8}
+          />
+        </label>
+        <div className="flex flex-col p-5">
+          {errors.name && (
+            <span className="text-red-500">- The Name Field is required</span>
+          )}
+          {errors.email && (
+            <span className="text-red-500">- The Email Field is required</span>
+          )}
+        </div>
+        <input
+          type="submit"
+          className={styles.form.submit[unit.branch.name as keyof object]}
+        />
+      </form>
     </main>
   );
 }
