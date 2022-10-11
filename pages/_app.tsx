458f8f41a1a89e4ad7fc14eea87514140609fca4
Agohige-Tech <60114461+Agohige-Tech@users.noreply.github.com>
2022-10-11 13:19:59 -0400

    Added in Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,29 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import { Session } from "next-auth";
+import Script from "next/script";
 
 function MyApp({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps<{ session: Session }>) {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script strategy="lazyOnload">
+        {`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});`}
+      </Script>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </>
   );
 }
 
