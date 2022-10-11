f0c5e5a50df2a3009b0d7450d45617d81d19ec2d
Agohige-Tech <60114461+Agohige-Tech@users.noreply.github.com>
2022-10-11 10:38:07 -0400

    updated revalidate

--- pages/unit/[slug].tsx
+++ pages/unit/[slug].tsx
@@ -321,6 +321,6 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     props: {
       unit,
     },
-    revalidate: 1800, // 86400 for after 1 day, updates old cached version...ISR implementation
+    revalidate: 86400, // 86400 for after 1 day, updates old cached version...ISR implementation
   };
 };
