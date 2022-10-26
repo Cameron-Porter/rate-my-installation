31d15c3b99a21ea9010dcd381995663f768ef1dc
Agohige-Tech <60114461+Agohige-Tech@users.noreply.github.com>
2022-10-25 22:07:56 -0400

    Editing validation back to 8hrs

--- pages/branch/[branchName]/unit/[slug].tsx
+++ pages/branch/[branchName]/unit/[slug].tsx
@@ -287,6 +287,6 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     props: {
       unit,
     },
-    revalidate: 30, // 28800 Revalidates data every 8hrs, updates old cached version...ISR implementation
+    revalidate: 28800, // Revalidates data every 8hrs, updates old cached version...ISR implementation
   };
 };
