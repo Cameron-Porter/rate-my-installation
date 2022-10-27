8d63b9f1d276efc5960f4f8ea3c60afafb0c977d
Agohige-Tech <60114461+Agohige-Tech@users.noreply.github.com>
2022-10-27 15:55:43 -0400

    fix(analytics): Removed GA scripts temporarily

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,22 +9,9 @@ function MyApp({
   pageProps: { session, ...pageProps },
 }: AppProps<{ session: Session }>) {
   return (
-    <>
-      <Script
-        strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-      />
-      <Script strategy="lazyOnload">
-        {`window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});`}
-      </Script>
-      <SessionProvider session={session}>
-        <Component {...pageProps} />
-      </SessionProvider>
-    </>
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
   );
 }
 
