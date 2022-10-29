4779820a76b17696db28fa62f0a688343fd88474
Agohige-Tech <60114461+Agohige-Tech@users.noreply.github.com>
2022-10-28 21:11:23 -0400

    feat(GA): Updated GA with additional content

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,15 +3,31 @@ import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import { Session } from "next-auth";
 import Script from "next/script";
+import { useGoogleTagsManager } from "../hooks";
 
 function MyApp({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps<{ session: Session }>) {
+  useGoogleTagsManager(process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS);
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+        `}
+      </Script>
+    </>
   );
 }
 
